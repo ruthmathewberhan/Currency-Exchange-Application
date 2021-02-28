ec5dde680af648731de5b012fdb7ebb6468504ea
Eden Tesfaye <se.eden.tesfaye@gmail.com>
2021-02-27 18:43:16 -0800

    added filter in latest html

--- asstes/js/latest.js
+++ asstes/js/latest.js
@@ -1,4 +1,8 @@
 const ul = document.querySelector("#rates");
+const filter = document.querySelector(
+    'body > div.ui.top.attached.tabular.menu > div > div > div > input[type=text]'
+);
+
 const apiurl = "https://api.currencyfreaks.com/latest?apikey=52d3089a00c148b9a91b94f0c6297484";
 let html = '';
 async function currency() {
@@ -10,10 +14,24 @@ async function currency() {
 
     // console.log(data);
     arrKeys.map(item => {
-        return (html += `<li>${item} : ${rates[item]}</li>`);
+        return (html += `<li class='i'>${item} : ${rates[item]}</li>`);
     });
     ul.innerHTML=html;
     // console.log(html);
 };
 
 currency();
+
+filter.addEventListener('keyup', (e) => {
+    const searchInput = e.target.value.toLowerCase();
+    // console.log(searchInput);
+
+    const items = document.getElementsByClassName('i');
+    Array.from(items).forEach((item) => {
+        if (!item.textContent.toLowerCase().startsWith(searchInput)) {
+            item.classList.add('hide');
+        } else {
+            item.classList.remove('hide');
+        }
+    });
+});
