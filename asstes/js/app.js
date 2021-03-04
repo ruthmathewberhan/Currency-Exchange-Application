520daf089a863ca830677a851c12bc35e0a43a65
Eden Tesfaye <se.eden.tesfaye@gmail.com>
2021-03-03 22:52:38 -0800

    added flag and symbol

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -1,33 +1,217 @@
-const select = document.querySelectorAll('select');
-const input = document.querySelectorAll('input');//returns an array [input0, input1]
+const addCurrencyBtn = document.querySelectorAll(".add-currency-btn");
+const addCurrencyList = document.querySelector(".add-currency-list");
+const currenciesList = document.querySelector(".currencies");
 const apiurl = "https://api.currencyfreaks.com/latest?apikey=52d3089a00c148b9a91b94f0c6297484";
-let html = '';
-async function currency() {
-    const res = await fetch(apiurl);
-    const data = await res.json();
 
-    const arrKeys = Object.keys(data.rates);
-    const rates = data.rates;
+const initiallyDisplayedCurrencies = ["ETB", "USD", "CAD", "CHF", "CNY"];
+let baseCurrency;
+let baseCurrencyAmount;
+
+let currencies = [
+{
+        name: "Ethiopian Birr",
+        abbreviation: "ETB",
+        symbol: "Br",
+        flagURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Flag_of_Ethiopia.svg/1024px-Flag_of_Ethiopia.svg.png"
+      },
+  {
+    name: "US Dollar",
+    abbreviation: "USD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/us.gif"
+  },
+  {
+    name: "Euro",
+    abbreviation: "EUR",
+    symbol: "\u20AC",
+    flagURL: "https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg"
+  },
+  {
+    name: "Japanese Yen",
+    abbreviation: "JPY",
+    symbol: "\u00A5",
+    flagURL: "http://www.geonames.org/flags/l/jp.gif"
+  },
+  {
+    name: "British Pound",
+    abbreviation: "GBP",
+    symbol: "\u00A3",
+    flagURL: "http://www.geonames.org/flags/l/uk.gif"
+  },
+  {
+    name: "Australian Dollar",
+    abbreviation: "AUD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/au.gif"
+  },
+  {
+    name: "Canadian Dollar",
+    abbreviation: "CAD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/ca.gif"
+  },
+  {
+    name: "Swiss Franc",
+    abbreviation: "CHF",
+    symbol: "\u0043\u0048\u0046",
+    flagURL: "http://www.geonames.org/flags/l/ch.gif"
+  },
+  {
+    name: "Chinese Yuan Renminbi",
+    abbreviation: "CNY",
+    symbol: "\u00A5",
+    flagURL: "http://www.geonames.org/flags/l/cn.gif"
+  },
+  {
+    name: "Swedish Krona",
+    abbreviation: "SEK",
+    symbol: "\u006B\u0072",
+    flagURL: "http://www.geonames.org/flags/l/se.gif"
+  },
+  {
+    name: "New Zealand Dollar",
+    abbreviation: "NZD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/nz.gif"
+  },
+  {
+    name: "Mexican Peso",
+    abbreviation: "MXN",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/mx.gif"
+  },
+  {
+    name: "Singapore Dollar",
+    abbreviation: "SGD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/sg.gif"
+  },
+  {
+    name: "Hong Kong Dollar",
+    abbreviation: "HKD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/hk.gif"
+  },
+  {
+    name: "Norwegian Krone",
+    abbreviation: "NOK",
+    symbol: "\u006B\u0072",
+    flagURL: "http://www.geonames.org/flags/l/no.gif"
+  },
+  {
+    name: "South Korean Won",
+    abbreviation: "KRW",
+    symbol: "\u20A9",
+    flagURL: "http://www.geonames.org/flags/l/kr.gif"
+  },
+  {
+    name: "Turkish Lira",
+    abbreviation: "TRY",
+    symbol: "\u20BA",
+    flagURL: "http://www.geonames.org/flags/l/tr.gif"
+  },
+  {
+    name: "Russian Ruble",
+    abbreviation: "RUB",
+    symbol: "\u20BD",
+    flagURL: "http://www.geonames.org/flags/l/ru.gif"
+  },
+  {
+    name: "Indian Rupee",
+    abbreviation: "INR",
+    symbol: "\u20B9",
+    flagURL: "http://www.geonames.org/flags/l/in.gif"
+  },
+  {
+    name: "Brazilian Real",
+    abbreviation: "BRL",
+    symbol: "\u0052\u0024",
+    flagURL: "http://www.geonames.org/flags/l/br.gif"
+  },
+  {
+    name: "South African Rand",
+    abbreviation: "ZAR",
+    symbol: "\u0052",
+    flagURL: "http://www.geonames.org/flags/l/za.gif"
+  },
+  {
+    name: "Philippine Peso",
+    abbreviation: "PHP",
+    symbol: "\u20B1",
+    flagURL: "http://www.geonames.org/flags/l/ph.gif"
+  },
+  {
+    name: "Czech Koruna",
+    abbreviation: "CZK",
+    symbol: "\u004B\u010D",
+    flagURL: "http://www.geonames.org/flags/l/cz.gif"
+  },
+  {
+    name: "Indonesian Rupiah",
+    abbreviation: "IDR",
+    symbol: "\u0052\u0070",
+    flagURL: "http://www.geonames.org/flags/l/id.gif"
+  },
+  {
+    name: "Malaysian Ringgit",
+    abbreviation: "MYR",
+    symbol: "\u0052\u004D",
+    flagURL: "http://www.geonames.org/flags/l/my.gif"
+  },
+  {
+    name: "Hungarian Forint",
+    abbreviation: "HUF",
+    symbol: "\u0046\u0074",
+    flagURL: "http://www.geonames.org/flags/l/hu.gif"
+  },
+  {
+    name: "Icelandic Krona",
+    abbreviation: "ISK",
+    symbol: "\u006B\u0072",
+    flagURL: "http://www.geonames.org/flags/l/is.gif"
+  },
+  {
+    name: "Croatian Kuna",
+    abbreviation: "HRK",
+    symbol: "\u006B\u006E",
+    flagURL: "http://www.geonames.org/flags/l/hr.gif"
+  },
+  {
+    name: "Bulgarian Lev",
+    abbreviation: "BGN",
+    symbol: "\u043B\u0432",
+    flagURL: "http://www.geonames.org/flags/l/bg.gif"
+  },
+  {
+    name: "Romanian Leu",
+    abbreviation: "RON",
+    symbol: "\u006C\u0065\u0069",
+    flagURL: "http://www.geonames.org/flags/l/ro.gif"
+  },
+  {
+    name: "Danish Krone",
+    abbreviation: "DKK",
+    symbol: "\u006B\u0072",
+    flagURL: "http://www.geonames.org/flags/l/dk.gif"
+  },
+  {
+    name: "Thai Baht",
+    abbreviation: "THB",
+    symbol: "\u0E3F",
+    flagURL: "http://www.geonames.org/flags/l/th.gif"
+  },
+  {
+    name: "Polish Zloty",
+    abbreviation: "PLN",
+    symbol: "\u007A\u0142",
+    flagURL: "http://www.geonames.org/flags/l/pl.gif"
+  },
+  {
+    name: "Israeli Shekel",
+    abbreviation: "ILS",
+    symbol: "\u20AA",
+    flagURL: "http://www.geonames.org/flags/l/il.gif"
+  }
+];
 
 
-    arrKeys.map(item => {
-        return html += `<option value=${item}>${item}</option>`;
-    });
-    for (let i = 0; i < select.length; i++) {
-        select[i].innerHTML = html;
-    };
-    function convert(i,j){
-        input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
-    }
-    // console.log(rates[select[1].value])
-    input[0].addEventListener('keyup', () =>convert(1,0))
-       //if input0 is selected edit the value of input1
-    input[1].addEventListener('keyup', () =>convert(0,1)) 
-  
-    select[0].addEventListener('change', ()=>convert(1,0))
-
-    select[1].addEventListener('change', ()=>convert(0,1))
-   
-};
-
-currency();
